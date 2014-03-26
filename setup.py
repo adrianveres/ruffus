ece7de54f5c58a99cc7fdd7826df735aed16acef
Adrian Veres <adrian.veres@gmail.com>
2014-03-25 23:46:36 -0400

    Include distribute_setup

--- setup.py
+++ setup.py
@@ -1,72 +1,72 @@
 #!/usr/bin/env python
-import ez_setup
-ez_setup.use_setuptools()
+#import ez_setup
+#ez_setup.use_setuptools()
+
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
 
 import sys, os
 if not sys.version_info[0:2] >= (2,4):
     sys.stderr.write("Requires Python later than 2.4\n")
     sys.exit(1)
-
+    
 # quickly import the latest version of ruffus
 sys.path.insert(0, os.path.abspath("."))
-import ruffus.ruffus_version
+#import ruffus.ruffus_version
 sys.path.pop(0)
-
-
+    
+    
 if sys.version_info[0:2] >= (2,6):
     module_dependencies = []
 else:
     module_dependencies = ['multiprocessing>=2.6', 'simplejson']
 
 
-from setuptools import setup, find_packages
+#from setuptools import setup, find_packages
 setup(
         name='ruffus',
-        version=ruffus.ruffus_version.__version, #major.minor[.patch[.sub]]
+        #version=ruffus.ruffus_version.__version, #major.minor[.patch[.sub]]
+        version='2.4b1',
         description='Light-weight Python Computational Pipeline Management',
-        maintainer="Leo Goodstadt",
-        maintainer_email="ruffus_lib@llew.org.uk",
-        author='Leo Goodstadt',
-        author_email='ruffus@llew.org.uk',
-        use_2to3=True,
         long_description=\
-"""
+"""     
 ***************************************
 Overview
 ***************************************
 
 
-    The ruffus module is a lightweight way to add support
+    The ruffus module is a lightweight way to add support 
     for running computational pipelines.
-
+    
     Computational pipelines are often conceptually quite simple, especially
     if we breakdown the process into simple stages, or separate **tasks**.
-
+    
     Each stage or **task** in a computational pipeline is represented by a python function
     Each python function can be called in parallel to run multiple **jobs**.
-
+    
     Ruffus was originally designed for use in bioinformatics to analyse multiple genome
     data sets.
 
 ***************************************
 Documentation
 ***************************************
-
-    Ruffus documentation can be found `here <http://www.ruffus.org.uk>`_ ,
-    with an `download notes <http://www.ruffus.org.uk/installation.html>`_ ,
-    a `short tutorial <http://http://www.ruffus.org.uk/tutorials/simple_tutorial/simple_tutorial.html>`_ and
-    an `in-depth tutorial <http://www.ruffus.org.uk/tutorials/manual/manual_introduction.html>`_ .
+    
+    Ruffus documentation can be found `here <http://wwwfgu.anat.ox.ac.uk/~lg/oss/ruffus/index.html>`_ ,
+    with an `download notes <http://wwwfgu.anat.ox.ac.uk/~lg/oss/ruffus/installation.html>`_ ,
+    a `short tutorial <http://wwwfgu.anat.ox.ac.uk/~lg/oss/ruffus/tutorials/simple_tutorial/simple_tutorial.html>`_ and
+    an `in-depth tutorial <http://wwwfgu.anat.ox.ac.uk/~lg/oss/ruffus/tutorials/manual/manual_introduction.html>`_ .
 
 
 ***************************************
 Background
 ***************************************
 
-    The purpose of a pipeline is to determine automatically which parts of a multi-stage
+    The purpose of a pipeline is to determine automatically which parts of a multi-stage 
     process needs to be run and in what order in order to reach an objective ("targets")
 
     Computational pipelines, especially for analysing large scientific datasets are
-    in widespread use.
+    in widespread use. 
     However, even a conceptually simple series of steps can be difficult to set up and
     to maintain, perhaps because the right tools are not available.
 
@@ -88,7 +88,7 @@ Features
     Automatic support for
 
         * Managing dependencies
-        * Parallel jobs
+        * Parallel jobs                         
         * Re-starting from arbitrary points, especially after errors
         * Display of the pipeline as a flowchart
         * Reporting
@@ -117,7 +117,7 @@ A Simple example
 
 
 
-        the ``@follows`` decorator indicate that the ``first_task`` function precedes ``second_task`` in
+        the ``@follows`` decorator indicate that the ``first_task`` function precedes ``second_task`` in 
         the pipeline.
 
 ********
@@ -126,11 +126,11 @@ Usage
 
     Each stage or **task** in a computational pipeline is represented by a python function
     Each python function can be called in parallel to run multiple **jobs**.
-
+    
     1. Import module::
-
+            
             import ruffus
-
+    
 
     1. Annotate functions with python decorators
 
@@ -155,13 +155,15 @@ Usage
 
 
 """,
-        url='http://www.ruffus.org.uk',
-        download_url = "https://code.google.com/p/ruffus/",
-
+        author='Leo Goodstadt',
+        author_email='ruffus@llew.org.uk',
+        url='http://ruffus.googlecode.com',
+        #download_url = "http://http://code.google.com/p/ruffus/download",
+    
         install_requires = module_dependencies, #['multiprocessing>=1.0', 'json' ], #, 'python>=2.5'],
         setup_requires   = module_dependencies, #['multiprocessing>=1.0', 'json'],    #, 'python>=2.5'],
 
-
+        
         classifiers=[
                     'Intended Audience :: End Users/Desktop',
                     'Development Status :: 5 - Production/Stable',
@@ -187,6 +189,7 @@ Usage
         packages=['ruffus'],
         package_dir={'ruffus': 'ruffus'},
         include_package_data = True,    # include everything in source control
+        use_2to3=True
         #package_data = {
         #    # If any package contains *.txt files, include them:
         #    '': ['*.TXT'],                                \
@@ -198,8 +201,8 @@ Usage
 #
 #  http://pypi.python.org/pypi
 #  http://docs.python.org/distutils/packageindex.html
-#
-#
-#
+#   
+# 
+# 
 # python setup.py register
 # python setup.py sdist --format=gztar upload
